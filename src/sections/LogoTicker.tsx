58db53faecc00128fe4e4b52c3a55459e0eccfb9
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-07-29 23:43:01 +0600

    logo ticker

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,37 @@
+import acmeLogo from '@/assets/logo-acme.png';
+import quantumLogo from '@/assets/logo-quantum.png';
+import echoLogo from '@/assets/logo-echo.png';
+import celestiaLogo from '@/assets/logo-celestial.png';
+import pulseLogo from '@/assets/logo-pulse.png';
+import apexLogo from '@/assets/logo-apex.png';
+import Image from 'next/image';
+
+const logos = [
+    acmeLogo,
+    quantumLogo,
+    echoLogo,
+    celestiaLogo,
+    pulseLogo,
+    apexLogo,
+];
+
 export const LogoTicker = () => {
-  return null;
+    return (
+        <div className="py-8 md:py-12 bg-white">
+            <div className="container">
+                <div className="flex overflow-hidden [mask-image:linear-gradient(to_right, transparent, black, transparent)]">
+                    <div className="flex gap-14 flex-none">
+                        {logos.map((logo, idx) => (
+                            <Image
+                                key={idx}
+                                src={logo}
+                                alt="logo"
+                                className="logo-ticker-img"
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
