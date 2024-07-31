20ae0b9cbce364c79b00e5118442ade49dcada23
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-07-31 23:40:26 +0600

    pricing section, fix bg-mask for logo tick

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -7,14 +7,14 @@ export const ProductShowcase = () => {
     return (
         <section className="bg-gradient-to-b from-[#fff] to-[#d2dcff] py-24 overflow-x-clip">
             <div className="container">
-                <div className="max-w-[540px] mx-auto">
+                <div className="section-heading">
                     <div className="flex justify-center">
                         <div className="tag">Boost Your productivity</div>
                     </div>
-                    <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-5">
+                    <h2 className="section-title mt-5">
                         A more effective way to track progress
                     </h2>
-                    <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010d3e] mt-5">
+                    <p className="section-description mt-5">
                         Effortlessly turn your ideas into a fully functional,
                         responsive, Saas website in jusy minutes with this
                         template
