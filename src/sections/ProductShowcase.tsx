73d5fed1e5b451804646589f526e5c966ca253d6
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-08-04 00:30:55 +0600

    product show case motion

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,23 @@
+'use client';
 import productImage from '@/assets/product-image.png';
 import Image from 'next/image';
 import pyramidImage from '@/assets/pyramid.png';
 import tubeImage from '@/assets/tube.png';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 export const ProductShowcase = () => {
+    const sectionRef = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: sectionRef,
+        offset: ['start end', 'end start'],
+    });
+    const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
     return (
-        <section className="bg-gradient-to-b from-[#fff] to-[#d2dcff] py-24 overflow-x-clip">
+        <section
+            ref={sectionRef}
+            className="bg-gradient-to-b from-[#fff] to-[#d2dcff] py-24 overflow-x-clip"
+        >
             <div className="container">
                 <div className="section-heading">
                     <div className="flex justify-center">
@@ -26,19 +38,21 @@ export const ProductShowcase = () => {
                         alt="Product Image"
                         className="mt-10"
                     />
-                    <Image
-                        src={pyramidImage}
+                    <motion.img
+                        src={pyramidImage.src}
                         alt="Pyramid Image"
                         className="hidden md:block absolute -right-36 -top-32"
                         height={262}
                         width={262}
+                        style={{ translateY }}
                     />
-                    <Image
-                        src={tubeImage}
+                    <motion.img
+                        src={tubeImage.src}
                         alt="Tube Image"
                         className="hidden md:block absolute bottom-24 -left-36"
                         height={248}
                         width={248}
+                        style={{ translateY }}
                     />
                 </div>
             </div>
