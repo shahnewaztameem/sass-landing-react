cde73c05c8e33ddf6e713a28cae51c71a4b9bce2
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-08-04 00:25:20 +0600

    logo slide

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+'use client';
 import acmeLogo from '@/assets/logo-acme.png';
 import quantumLogo from '@/assets/logo-quantum.png';
 import echoLogo from '@/assets/logo-echo.png';
@@ -5,7 +6,7 @@ import celestiaLogo from '@/assets/logo-celestial.png';
 import pulseLogo from '@/assets/logo-pulse.png';
 import apexLogo from '@/assets/logo-apex.png';
 import Image from 'next/image';
-
+import { motion } from 'framer-motion';
 const logos = [
     acmeLogo,
     quantumLogo,
@@ -20,7 +21,18 @@ export const LogoTicker = () => {
         <div className="py-8 md:py-12 bg-white">
             <div className="container">
                 <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-                    <div className="flex gap-14 flex-none">
+                    <motion.div
+                        className="flex gap-14 flex-none pr-14"
+                        animate={{
+                            translateX: '-50%',
+                        }}
+                        transition={{
+                            duration: 20,
+                            repeat: Infinity,
+                            ease: 'linear',
+                            repeatType: 'loop',
+                        }}
+                    >
                         {logos.map((logo, idx) => (
                             <Image
                                 key={idx}
@@ -29,7 +41,15 @@ export const LogoTicker = () => {
                                 className="logo-ticker-img"
                             />
                         ))}
-                    </div>
+                        {logos.map((logo, idx) => (
+                            <Image
+                                key={idx}
+                                src={logo}
+                                alt="logo"
+                                className="logo-ticker-img"
+                            />
+                        ))}
+                    </motion.div>
                 </div>
             </div>
         </div>
