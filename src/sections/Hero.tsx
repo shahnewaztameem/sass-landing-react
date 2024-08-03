72b3395a4aa88bc8d81066f695a629af9b7c1b21
Shahnewaz <gazi.shahnewaz@apsissolutions.com>
2024-08-04 00:11:56 +0600

    text animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -22,26 +22,87 @@ export const Hero = () => {
         >
             <div className="container">
                 <div className="md:flex items-center">
-                    <div className="md:w-[478px]">
-                        <div className="tag">Version 1.0 is Here</div>
-                        <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
-                            Pathway to productivity
-                        </h1>
-                        <p className="text-xl text-[#010D3E] tracking-tight mt-6">
-                            Celebrate the joy of accomplishment with an app
-                            designed to track your progress, motivate your
-                            effortsm and celebrate your successes.
-                        </p>
-                        <div className="flex gap-1 items-center mt-[30px]">
-                            <button className="btn btn-primary">
-                                Get Started
-                            </button>
-                            <button className="btn btn-text gap-1">
-                                <span>Learn More</span>
-                                <ArrowRight className="h-5 w-5" />
-                            </button>
-                        </div>
-                    </div>
+                    <motion.div
+                        initial={{ opacity: 0, y: -20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{
+                            duration: 0.6,
+                            ease: 'easeOut',
+                            delay: 0.2,
+                        }}
+                        className="md:flex items-center"
+                    >
+                        <motion.div
+                            initial={{ opacity: 0, y: -20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{
+                                duration: 0.6,
+                                ease: 'easeOut',
+                                delay: 0.2,
+                            }}
+                            className="md:w-[478px]"
+                        >
+                            <motion.div
+                                initial={{ opacity: 0, y: -20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    duration: 0.6,
+                                    ease: 'easeOut',
+                                    delay: 0.4,
+                                }}
+                                className="tag"
+                            >
+                                Version 1.0 is Here
+                            </motion.div>
+
+                            <motion.h1
+                                initial={{ opacity: 0, y: -20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    duration: 0.6,
+                                    ease: 'easeOut',
+                                    delay: 0.6,
+                                }}
+                                className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6"
+                            >
+                                Pathway to productivity
+                            </motion.h1>
+
+                            <motion.p
+                                initial={{ opacity: 0, y: -20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    duration: 0.6,
+                                    ease: 'easeOut',
+                                    delay: 0.8,
+                                }}
+                                className="text-xl text-[#010D3E] tracking-tight mt-6"
+                            >
+                                Celebrate the joy of accomplishment with an app
+                                designed to track your progress, motivate your
+                                efforts, and celebrate your successes.
+                            </motion.p>
+
+                            <motion.div
+                                initial={{ opacity: 0, y: -20 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    duration: 0.6,
+                                    ease: 'easeOut',
+                                    delay: 1.0,
+                                }}
+                                className="flex gap-1 items-center mt-[30px]"
+                            >
+                                <button className="btn btn-primary">
+                                    Get Started
+                                </button>
+                                <button className="btn btn-text gap-1">
+                                    <span>Learn More</span>
+                                    <ArrowRight className="h-5 w-5" />
+                                </button>
+                            </motion.div>
+                        </motion.div>
+                    </motion.div>
                     <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
                         <motion.img
                             src={cogImage.src}
